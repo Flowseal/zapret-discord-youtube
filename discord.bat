65670a4d71431e379b4bffd49e2bd13364020bf6
Shurik24 <shurik24@night-watchers.com>
2024-10-09 10:26:46 +0200

    Added checks for Cyrillic, spaces, and admin rights Added some messages for users

--- discord.bat
+++ discord.bat
@@ -1,3 +1,34 @@
+@echo off
+chcp 65001 >nul
+:: 65001 - UTF-8
+
+:: Path check
+set scriptPath=%~dp0
+set "path_no_spaces=%scriptPath: =%"
+if not "%scriptPath%"=="%path_no_spaces%" (
+    echo Путь содержит пробелы. 
+    echo Пожалуйста, переместите скрипт в директорию без пробелов.
+    pause
+    exit /b
+)
+
+:: Cyrillic check
+echo %scriptPath% | findstr /r "[А-Яа-яЁё]" >nul
+if %errorLevel% equ 0 (
+    echo Путь содержит кирилицу. Пожалуйста, переместите скрипт в директорию без кириллических символов.
+    echo Кириллица - Русский алфавит.
+    pause
+    exit /b
+)
+
+:: Admin rights check
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Запуск от имени администратора...
+    powershell start -verb runas '%0'
+    exit /b
+)
+
 start "zapret: discord" /min "%~dp0winws.exe" ^
 --wf-tcp=443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="%~dp0list-discord.txt" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
--- service_remove.bat
+++ service_remove.bat
@@ -1,4 +1,40 @@
+@echo off
+chcp 65001 >nul
+:: 65001 - UTF-8
+
+:: Path check
+set scriptPath=%~dp0
+set "path_no_spaces=%scriptPath: =%"
+if not "%scriptPath%"=="%path_no_spaces%" (
+    echo Путь содержит пробелы. 
+    echo Пожалуйста, переместите скрипт в директорию без пробелов.
+    pause
+    exit /b
+)
+
+:: Cyrillic check
+echo %scriptPath% | findstr /r "[А-Яа-яЁё]" >nul
+if %errorLevel% equ 0 (
+    echo Путь содержит кирилицу. Пожалуйста, переместите скрипт в директорию без кириллических символов.
+    echo Кириллица - Русский алфавит.
+    pause
+    exit /b
+)
+
+:: Admin rights check
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Запуск от имени администратора...
+    powershell start -verb runas '%0'
+    exit /b
+)
+
+
 set SRVCNAME=zapret
 
 net stop "%SRVCNAME%"
 sc delete "%SRVCNAME%"
+
+echo Сервис остановлен и удален.
+echo Если какой либо файл не удаляется, перезагрузите пк.
+pause
