86ae95a11840b91120da658de7544b0d976d331b
Shurik24 <shurik24@night-watchers.com>
2024-10-09 14:57:29 +0200

    Removed cyrillic check

--- discord_youtube.bat
+++ discord_youtube.bat
@@ -12,26 +12,6 @@ if not "%scriptPath%"=="%path_no_spaces%" (
     exit /b
 )
 
-:: Cyrillic check
-setlocal enabledelayedexpansion
-set "cyrillic_found=0"
-for /l %%i in (0,1,127) do (
-    set "char=!scriptPath:~%%i,1!"
-    for %%c in (А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я) do (
-        if "!char!"=="%%c" set "cyrillic_found=1"
-    )
-)
-:: This is only way what i found to check if cyrillic character is in string
-:: If you know better way, please let me know
-
-if %cyrillic_found% equ 1 (
-    echo Путь содержит кириллицу. 
-    echo Пожалуйста, переместите скрипт в директорию без кириллических символов.
-    echo Кириллица - Русский алфавит.
-    pause
-    exit /b
-)
-
 start "zapret: discord" /min "%~dp0winws.exe" ^
 --wf-tcp=80,443,50000-65535 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist="%~dp0list-general.txt" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
--- service_discord_youtube.bat
+++ service_discord_youtube.bat
@@ -12,26 +12,6 @@ if not "%scriptPath%"=="%path_no_spaces%" (
     exit /b
 )
 
-:: Cyrillic check
-setlocal enabledelayedexpansion
-set "cyrillic_found=0"
-for /l %%i in (0,1,127) do (
-    set "char=!scriptPath:~%%i,1!"
-    for %%c in (А Б В Г Д Е Ё Ж З И Й К Л М Н О П Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я а б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я) do (
-        if "!char!"=="%%c" set "cyrillic_found=1"
-    )
-)
-:: This is only way what i found to check if cyrillic character is in string
-:: If you know better way, please let me know
-
-if %cyrillic_found% equ 1 (
-    echo Путь содержит кириллицу. 
-    echo Пожалуйста, переместите скрипт в директорию без кириллических символов.
-    echo Кириллица - Русский алфавит.
-    pause
-    exit /b
-)
-
 :: Admin rights check
 net session >nul 2>&1
 if %errorLevel% neq 0 (
