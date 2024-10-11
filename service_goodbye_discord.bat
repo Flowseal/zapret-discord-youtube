c0b4799aa3fe828084af8ffdb9a8a8e793e9bb0b
Flowseal <sellyalegium@gmail.com>
2024-10-12 01:25:26 +0300

    cutoff for 3 packets

--- service_goodbye_discord.bat
+++ service_goodbye_discord.bat
@@ -10,7 +10,7 @@ pause
 set BIN=%~dp0bin\
 set ARGS=--wf-tcp=443 --wf-udp=443,50000-65535 ^
 --filter-udp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
---filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
+--filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d4 --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
 --filter-tcp=443 --hostlist=\"%~dp0list-discord.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=\"%BIN%tls_clienthello_www_google_com.bin\"
 
 set SRVCNAME=zapret
