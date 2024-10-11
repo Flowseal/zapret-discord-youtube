d755e5596b020a3bab5d5e170c80e52348c18c9e
KG7x <Cados7x@gmail.com>
2024-10-11 23:56:21 +0300

    Реальная проверка прав администратора

--- service_discord_youtube.bat
+++ service_discord_youtube.bat
@@ -2,10 +2,18 @@
 chcp 65001 >nul
 :: 65001 - UTF-8
 
-:: Admin rights check
-echo Данный файл должен быть запущен с правами администратора (ПКМ - Запустить от имени администратора).
-echo Нажмите любую клавишу, чтобы продолжить создание сервиса.
-pause
+whoami /groups | findstr "S-1-16-12288" >nul && goto :admin
+if "%~1"=="RunAsAdmin" goto :error
+
+:error
+echo.
+echo. Данный файл должен быть запущен с правами администратора (ПКМ - Запустить от имени администратора).
+echo. Чтобы выполнить создание сервиса.
+echo.
+goto :end
+
+:admin
+
 
 set BIN=%~dp0bin\
 set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
@@ -22,4 +30,7 @@ sc create %SRVCNAME% binPath= "\"%BIN%winws.exe\" %ARGS%" DisplayName= "zapret D
 sc description %SRVCNAME% "zapret DPI bypass software"
 sc start %SRVCNAME%
 
+
+:end
 pause
+exit /b
--- service_remove.bat
+++ service_remove.bat
@@ -2,14 +2,25 @@
 chcp 65001 >nul
 :: 65001 - UTF-8
 
-:: Admin rights check
-echo Данный файл должен быть запущен с правами администратора (ПКМ - Запустить от имени администратора).
-echo Нажмите любую клавишу, чтобы продолжить удаление и остановку сервиса.
-pause
+whoami /groups | findstr "S-1-16-12288" >nul && goto :admin
+if "%~1"=="RunAsAdmin" goto :error
+
+:error
+echo.
+echo. Данный файл должен быть запущен с правами администратора (ПКМ - Запустить от имени администратора).
+echo. Чтобы выполнить удаление и остановку сервиса.
+echo.
+goto :end
+
+:admin
+
 
 set SRVCNAME=zapret
 
 net stop %SRVCNAME%
 sc delete %SRVCNAME%
 
+
+:end
 pause
+exit /b
