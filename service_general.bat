d39d1050d91c4e59b4b005b318c3f1afc46aef7e
gepron1x <56407064+gepron1x@users.noreply.github.com>
2024-10-15 02:53:51 +0300

    Use custom windivert filter for discord (#294)
    * Use custom windivert filter for discord
    
    * Remove unnecessary %~dp0
    
    * escape quotes

--- service_general.bat
+++ service_general.bat
@@ -8,7 +8,7 @@ echo Нажмите любую клавишу, чтобы продолжить �
 pause
 
 set BIN=%~dp0bin\
-set ARGS=--wf-tcp=80,443 --wf-udp=443,50000-65535 ^
+set ARGS=--wf-raw=\"@%~dp0rules.txt\" ^
 --filter-udp=443 --hostlist=\"%~dp0list-general.txt\" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic=\"%BIN%quic_initial_www_google_com.bin\" --new ^
 --filter-tcp=80 --hostlist=\"%~dp0list-general.txt\" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
