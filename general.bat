246716316f278ffe8cdeebbd1cf0cef2d6f379e2
gepron1x <georgijpronuk@gmail.com>
2024-10-15 02:39:45 +0300

    Remove unnecessary %~dp0

--- general.bat
+++ general.bat
@@ -6,7 +6,7 @@ cd /d "%~dp0"
 
 set BIN=%~dp0bin\
 
-start "zapret: general" /min "%BIN%winws.exe" --wf-raw="@%~dp0rules.txt" ^
+start "zapret: general" /min "%BIN%winws.exe" --wf-raw="@rules.txt" ^
 --filter-udp=443 --hostlist="list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-udplen-increment=10 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
 --filter-tcp=80 --hostlist="list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
