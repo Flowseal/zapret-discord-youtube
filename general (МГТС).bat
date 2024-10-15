5c35c8810ce9ea5aae87619b0f632d4eb270643a
Flowseal <sellyalegium@gmail.com>
2024-10-16 01:44:28 +0300

    alternative strategies

---
+++ general (МГТС).bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001 >nul
+:: 65001 - UTF-8
+
+cd /d "%~dp0"
+
+set BIN=%~dp0bin\
+
+start "zapret: general" /min "%BIN%winws.exe" --wf-raw="@rules.txt" ^
+--filter-udp=443 --hostlist="list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-udp=50000-65535 --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-tcp=80 --hostlist="list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
+--filter-tcp=443 --hostlist="list-general.txt" --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin"
