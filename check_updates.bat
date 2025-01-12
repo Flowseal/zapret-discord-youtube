7656ba101eb63c927b93817bfe28de36b0286aba
Flowseal <sellyalegium@gmail.com>
2025-01-12 08:49:07 +0300

    v1.6.2

--- check_updates.bat
+++ check_updates.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 chcp 437 > nul
 
-set "CURRENT_VERSION=1.6.1"
+set "CURRENT_VERSION=1.6.2"
 set "GITHUB_URL=https://raw.githubusercontent.com/Flowseal/zapret-discord-youtube/refs/heads/main/.service/version.txt"
 set "RELEASE_URL=https://github.com/Flowseal/zapret-discord-youtube/releases"
 set "VERSION_FILE=version.txt"
