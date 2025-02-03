5f0035de619b22b44514251fb12b6972ef0c2d3d
amozebus <amozebus@gmail.com>
2025-02-03 17:34:58 +0600

    administrator rights check remake

--- service_remove.bat
+++ service_remove.bat
@@ -2,12 +2,13 @@
 chcp 65001 >nul
 :: 65001 - UTF-8
 
-set "arg=%1"
-if "%arg%" == "admin" (
-    echo Restarted with admin rights
-) else (
-    powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/k \"\"%~f0\" admin\"' -Verb RunAs"
-    exit /b
+net session >nul 2>&1
+
+if not %errorLevel% == 0 (
+   echo Started NOT as administrator. Press any key to start as administrator...
+   pause >nul
+   powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/k \"\"%~f0\" admin\"' -Verb RunAs"
+   exit /b
 )
 
 set SRVCNAME=zapret
