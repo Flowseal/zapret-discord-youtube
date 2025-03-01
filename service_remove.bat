2b16cb4444f18385ab0c9bf0b56f682489570581
fridorin <fridoriny@gmail.com>
2025-03-01 12:54:12 +0300

    downgrade admin rights check
    вернул все обратно из за https://github.com/Flowseal/zapret-discord-youtube/pull/1358

--- service_remove.bat
+++ service_remove.bat
@@ -2,13 +2,13 @@
 chcp 65001 > nul
 :: 65001 - UTF-8
 
-net session >nul 2>&1
-if %errorLevel% neq 0 (
+if "%1"=="admin" (
+    echo Started with admin rights
+) else (
     echo Requesting admin rights...
     powershell -Command "Start-Process 'cmd.exe' -ArgumentList '/k \"\"%~f0\" admin\"' -Verb RunAs"
     exit /b
 )
-if "%1"=="admin" echo Started with admin rights
 
 set SRVCNAME=zapret
 
