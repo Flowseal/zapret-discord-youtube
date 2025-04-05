22e9f55b7388afe1c06597d6a1494e4ba0b6c5e1
Kleveriwe <noobsaibotterrach@ya.ru>
2025-04-05 10:34:21 +0300

    Добавлены пресеты и универсальный launcher.bat

---
+++ profiles/profile_launcher.bat
@@ -0,0 +1,45 @@
+@echo off
+chcp 65001 > nul
+setlocal EnableDelayedExpansion
+
+:: Проверка аргумента
+if "%~1"=="" (
+    echo [ERROR] Не передан путь к параметрам. Пример: call profile_launcher.bat presets\winws-params-alt1.txt
+    exit /b 1
+)
+
+:: Переход в корень
+cd /d "%~dp0\.."
+
+:: Пути
+set "BIN=%~dp0..\bin\"
+set "PARAMS=%~dp0..\%~1"
+
+:: Проверка winws.exe и параметров
+if not exist "%BIN%winws.exe" (
+    echo [ERROR] winws.exe не найден!
+    exit /b 1
+)
+
+if not exist "%PARAMS%" (
+    echo [ERROR] Параметры %PARAMS% не найдены!
+    exit /b 1
+)
+
+:: Проверка, не запущен ли уже winws.exe
+tasklist /FI "IMAGENAME eq winws.exe" | find /I "winws.exe" > nul
+if not errorlevel 1 (
+    echo winws.exe уже работает.
+    exit /b 0
+)
+
+:: Проверка службы и обновлений
+call service\service_status.bat zapret
+call updater\check_updates.bat soft
+
+:: Запуск winws.exe
+start "zapret" /min cmd /c ""%BIN%winws.exe" @"%PARAMS%"" 
+
+
+
+exit /b 0
