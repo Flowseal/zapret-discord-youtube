38de0a4f85017426d667732ee99985fcd6420638
lxstwxrden <48512277+V3nilla@users.noreply.github.com>
2025-04-12 02:39:08 +0400

    Update discord.bat

--- discord.bat
+++ discord.bat
@@ -12,5 +12,5 @@ set LISTS=%~dp0lists\
 
 start "zapret: discord" /min "%BIN%winws.exe" --wf-tcp=443 --wf-udp=443,50000-50100 ^
 --filter-udp=443 --hostlist="%LISTS%list-discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
---filter-udp=50000-50100 --ipset="%LISTS%ipset-discord.txt" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
+--filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --new ^
 --filter-tcp=443 --hostlist="%LISTS%list-discord.txt" --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-autottl --dpi-desync-fooling=badseq --dpi-desync-repeats=8
