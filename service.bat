c0d9a525510f40c105c396c5c521579686859ba5
JustChickNugget <belodedmatvey3@gmail.com>
2025-12-29 16:01:12 +0700

    Added an option to return to the menu

--- service.bat
+++ service.bat
@@ -209,6 +209,8 @@ for /f "delims=" %%F in ('powershell -NoProfile -Command "Get-ChildItem -Literal
     set "file!count!=%%F"
 )
 
+echo 0. Return Back
+
 :: Choosing file
 set "choice="
 set /p "choice=Input file index (number): "
@@ -217,6 +219,8 @@ if "!choice!"=="" (
     pause
     goto menu
 )
+
+if "!choice!"=="0" goto menu
 
 set "selectedFile=!file%choice%!"
 if not defined selectedFile (
