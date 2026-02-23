59206314401fee5d39341cd3c992d61e8d055804
Flowseal <sellyalegium@gmail.com>
2026-02-23 15:38:57 +0300

    Backward gamefilter compatability

--- service.bat
+++ service.bat
@@ -707,20 +707,20 @@ for /f "usebackq delims=" %%A in ("%gameFlagFile%") do (
     if not defined GameFilterMode set "GameFilterMode=%%A"
 )
 
-if /i "%GameFilterMode%"=="tcp" (
+if /i "%GameFilterMode%"=="all" (
+    set "GameFilterStatus=enabled (TCP and UDP)"
+    set "GameFilter=1024-65535"
+    set "GameFilterTCP=1024-65535"
+    set "GameFilterUDP=1024-65535"
+) else if /i "%GameFilterMode%"=="tcp" (
     set "GameFilterStatus=enabled (TCP)"
     set "GameFilter=1024-65535"
     set "GameFilterTCP=1024-65535"
     set "GameFilterUDP=12"
-) else if /i "%GameFilterMode%"=="udp" (
+) else (
     set "GameFilterStatus=enabled (UDP)"
     set "GameFilter=1024-65535"
     set "GameFilterTCP=12"
-    set "GameFilterUDP=1024-65535"
-) else (
-    set "GameFilterStatus=enabled (TCP and UDP)"
-    set "GameFilter=1024-65535"
-    set "GameFilterTCP=1024-65535"
     set "GameFilterUDP=1024-65535"
 )
 exit /b
