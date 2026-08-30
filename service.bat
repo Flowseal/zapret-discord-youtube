f1f17d4d97e0c4cb2aeca565d660da0e541959c7
phantom54 <35395849+phantom54@users.noreply.github.com>
2026-08-30 21:27:41 +0700

    Service.bat: GameFilterUDP exclude 7770 port for Dead by Daylight
    Update Service.bat: exclude port 7770 to fix Dead by Daylight region selection issues for matchmaking

--- service.bat
+++ service.bat
@@ -749,7 +749,7 @@ if /i "%GameFilterMode%"=="all" (
     set "GameFilterStatus=enabled (TCP and UDP)"
     set "GameFilter=1024-65535"
     set "GameFilterTCP=1024-65535"
-    set "GameFilterUDP=1024-65535"
+    set "GameFilterUDP=1024-7769,7771-65535"
 ) else if /i "%GameFilterMode%"=="tcp" (
     set "GameFilterStatus=enabled (TCP)"
     set "GameFilter=1024-65535"
@@ -759,7 +759,7 @@ if /i "%GameFilterMode%"=="all" (
     set "GameFilterStatus=enabled (UDP)"
     set "GameFilter=1024-65535"
     set "GameFilterTCP=12"
-    set "GameFilterUDP=1024-65535"
+    set "GameFilterUDP=1024-7769,7771-65535"
 )
 exit /b
 
